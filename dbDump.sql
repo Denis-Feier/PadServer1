1e7ff828fc60bcc5c56eb78978e2b089300cafd7
DenisuDevForFood <denisfeier98@yahoo.com>
2020-04-16 23:21:35 +0300

    new products added in DB

--- dbDump.sql
+++ dbDump.sql
@@ -49,4 +49,13 @@ VALUES
        (5, 'Prosciutto e Funghi', 25,
         'aluat de pizza,sos de rosii 5 linguri,mozzarella 70g,prosciutto sau sunca 6 felii,ciuperci 60g,masline 4bucati - optional,oregano uscat 1 lingurita',
         450, 1,
-        'http://urgentpizza.ro/blog/wp-content/uploads/2014/05/prosciutto-e-funghi-urgenpizza-575x262.png');
+        'http://urgentpizza.ro/blog/wp-content/uploads/2014/05/prosciutto-e-funghi-urgenpizza-575x262.png'),
+       (6, 'Pizza Vegetariana', 24.5, 'roșii tăiate cubulețe, pastă de tomate, ceapă, oregano uscat, ardei gras verde',
+        600, 1, 'https://www.lalena.ro/images/uploaded/600x_Pizza-Vegetariana-ca-la-pizzerie-632.jpg'),
+       (7, 'Pizza Fructe De Mare', 26.0, ' faina alba, drojdie de bere uscata, lingurita de zahar,  praf de pier negru, sare fina',
+        600, 1, 'https://atelieruldepizza.ro/wp-content/uploads/2017/02/REC_PRO_0033.png'),
+       (8, 'Pizza Cu Ton', 20, 'faina, drojdie, ulei de masline, sare, ceapa rosie, mozzarella pentru pizza, masline negre, oregano uscat',
+        600, 1, 'https://2.bp.blogspot.com/-vQGXL1viC70/UjAh5mZMM9I/AAAAAAAAUG0/6csGimdiLTo/s1600/DSCF8750.jpg'),
+       (9, 'Burger De Pui', 25, 'piept de pui dezosat, ceapă roșie,  ulei de masline, piper, sare', 600, 1, 'https://pizzahugo.ro/wp-content/uploads/2018/11/burger-de-pui-detaliu-min.jpg'),
+       (10, 'Burger Vegetarian', 20, 'usturoi, ceapa rosie tocata, zeama de lamaie, chifle din faina integrala, sare si piper',
+        600, 1, 'https://bobulverde.eu/wp-content/uploads/2018/06/burger-vegan-1440x960.jpg');
