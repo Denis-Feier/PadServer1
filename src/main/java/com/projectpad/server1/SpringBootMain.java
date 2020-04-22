e003194b765b3794cb392303ea9ae34c1437d4ec
DenisuDevForFood <denisfeier98@yahoo.com>
2020-04-22 18:24:42 +0300

    cors filter added

--- src/main/java/com/projectpad/server1/SpringBootMain.java
+++ src/main/java/com/projectpad/server1/SpringBootMain.java
@@ -2,6 +2,8 @@ package com.projectpad.server1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringBootMain {
@@ -10,4 +12,13 @@ public class SpringBootMain {
         SpringApplication.run(SpringBootMain.class, args);
     }
 
+    @Bean
+    public FilterRegistrationBean corsFilterRegistration() {
+        FilterRegistrationBean registrationBean =
+                new FilterRegistrationBean(new CORSFilter());
+        registrationBean.setName("CORS Filter");
+        registrationBean.addUrlPatterns("/*");
+        registrationBean.setOrder(1);
+        return registrationBean;
+    }
 }
